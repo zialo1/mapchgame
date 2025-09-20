28b273d39854b7468048bf2f21e32662fc834467
Aha80 <62428569+zialo1@users.noreply.github.com>
2025-09-20 23:17:45 +0200

    fixed FONT in theme.json

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #!python
-# mapchgame
+# mapchgame: eine einfache klickbare Karte. Zeigt Kantonsnamen beim anklicken an.
+# (C) 2025 Alex Hanselmann, code with help of ChatGPT
+# version 0.1 - 2025-09-20
 # benötigt: pyshp, pygame, pygame_gui
-# pip install pyshp pygame pygame_gui
-# (C) 2025 Alex Hanselmann
 # Lizenz: MIT
 # Geo-Daten: swisstopo (swissBOUNDARIES3D)
 # https://www.swisstopo.admin.ch/de/geodata/landscape/boundaries
@@ -10,59 +10,113 @@
 import shapefile
 import pygame
 import pygame_gui
-import math
+from dataclasses import dataclass
 import time
 
-
 # -------- Konfiguration --------
-SHP_PATH = "swissBOUNDARIES3D_1_5_TLM_KANTONSGEBIET.shp"
-MAP_PATH = "swissmap770.png"
-mappos=(29,-29)
-
-kantone =[
-'Genf',
-'Thurgau',
-'Wallis',
-'Aargau',
-'Schwyz',
-'Zürich',
-'Obwalden',
-'Freiburg',
-'Glarus',
-'Uri',
-'Nidwalden',
-'Solothurn',
-'Appenzell Ausserrhoden',
-'Jura',
-'Graubünden',
-'Waadt',
-'Luzern',
-'Tessin',
-'Zug',
-'Baselland',
-'Sankt Gallen',
-'Schaffhausen',
-'Bern',
-'Baselstadt',
-'Neuenburg',
-'Appenzell Innerrhoden'
-]
 
 
-WINDOW_SIZE = (800, 600)
-BG_COLOR = (255, 255, 255)
-NORMAL_FILL = (0, 150, 200)
-HOVER_FILL = (135, 206, 250)
-OUTLINE = (0, 0, 0)
+@dataclass
+class myappdatac:
+    SHP_PATH = "swissBOUNDARIES3D_1_5_TLM_KANTONSGEBIET.shp"
+    MAP_PATH = "swissmap770.png"
+    mappos = (29, -29)
 
-# Skala / Offset anpassen
-SCALE = 0.002
-OFFSET = (-4900, 2600)
+    shplabels_kantone =[
+    'Genf',
+    'Thurgau',
+    'Wallis',
+    'Aargau',
+    'Schwyz',
+    'Zürich',
+    'Obwalden',
+    'Freiburg',
+    'Glarus',
+    'Uri',
+    'Nidwalden',
+    'Solothurn',
+    'Appenzell Ausserrhoden',
+    'Jura',
+    'Graubünden',
+    'Waadt',
+    'Luzern',
+    'Tessin',
+    'Zug',
+    'Baselland',
+    'Sankt Gallen',
+    'Schaffhausen',
+    'Bern',
+    'Baselstadt',
+    'Neuenburg',
+    'Appenzell Innerrhoden']
 
-# -------- Animation-Parameter --------
-ANIM_DURATION = 0.2  # sek
-SCALE_UP = 1.2
-SCALE_DOWN = 1
+    WINDOW_SIZE = (800, 600)
+    BG_COLOR = (255, 255, 255)
+    NORMAL_FILL = (0, 150, 200)
+    HOVER_FILL = (135, 206, 250)
+    OUTLINE = (0, 0, 0)
+
+    # Skala / Offset anpassen
+    SCALE = 0.002
+    OFFSET = (-4900, 2600)
+
+    # -------- Animation-Parameter --------
+    ANIM_DURATION = 0.2  # sek
+    SCALE_UP = 1.2
+    SCALE_DOWN = 1
+    
+    # COLORS
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
+    GRAY = (200, 200, 200, 0.3)
+
+
+class compassrose:
+    def __init__(self,screen,apos,size=80):
+
+    # Mittelpunkt
+        self.size = size  # Länge der Spitzen
+
+        cx,cy = apos[0],apos[1]
+        quarter = int(size/4)
+        ten = int(size/8)
+    # Polygonzüge (je ein Dreieck für jede Richtung)
+        self.north = [(cx, cy - size), (cx - quarter, cy), (cx + quarter, cy)]
+        self.south = [(cx, cy + size), (cx - quarter, cy), (cx + quarter, cy)]
+        self.east  = [(cx + size, cy), (cx, cy - quarter), (cx, cy + quarter)]
+        self.west  = [(cx - size, cy), (cx, cy - quarter), (cx, cy + quarter)]
+
+    # Schrift
+        self.font = pygame.font.SysFont("arial", 24, bold=True)
+
+        self.labels = {
+            "N": (cx, cy - size - quarter),
+            "S": (cx, cy + size + quarter+1),
+            "E": (cx + size + ten+2, cy ),
+            "W": (cx - size - quarter-2, cy )
+        }
+        self.screen = screen
+
+    def draw(self):
+            # Kompassdreiecke zeichnen
+        pygame.draw.polygon(self.screen, myappdatac.GRAY, self.north)
+        pygame.draw.polygon(self.screen, myappdatac.GRAY, self.south)
+        pygame.draw.polygon(self.screen, myappdatac.GRAY, self.east)
+        pygame.draw.polygon(self.screen, myappdatac.GRAY, self.west)
+
+        # Randlinien
+        pygame.draw.polygon(self.screen, myappdatac.BLACK, self.north, 2)
+        pygame.draw.polygon(self.screen, myappdatac.BLACK, self.south, 2)
+        pygame.draw.polygon(self.screen, myappdatac.BLACK, self.east, 2)
+        pygame.draw.polygon(self.screen, myappdatac.BLACK, self.west, 2)
+
+        # Buchstaben zeichnen
+        for letter, pos in self.labels.items():
+            text_surf = self.font.render(letter, True, myappdatac.BLACK)
+            text_rect = text_surf.get_rect(center=pos)
+            self.screen.blit(text_surf, text_rect)
+
+
 
 def start_animation(shape_data, new_target):
     shape_data["anim_from"] = shape_data["current_scale"]
@@ -73,7 +127,7 @@ def update_animations():
     now = time.time()
     for s in shapes_data:
         if s["anim_start"] is not None:
-            t = (now - s["anim_start"]) / ANIM_DURATION
+            t = (now - s["anim_start"]) / myappdatac.ANIM_DURATION
             if t >= 1.0:
                 s["current_scale"] = s["target_scale"]
                 s["anim_start"] = None
@@ -82,7 +136,7 @@ def update_animations():
                 f = s["anim_from"] + (s["target_scale"] - s["anim_from"]) * t
                 s["current_scale"] = f
 
-def draw_shape(surface, shape_data, fill_color, bg_color=BG_COLOR):
+def draw_shape(surface, shape_data, fill_color, bg_color=myappdatac.BG_COLOR):
     scale = shape_data["current_scale"]
     # Skaliere jedes Teil um seinen Schwerpunkt
     for p in shape_data["parts"]:
@@ -94,13 +148,13 @@ def draw_shape(surface, shape_data, fill_color, bg_color=BG_COLOR):
             ]
             if p["type"] == "outer":
                 pygame.draw.polygon(surface, fill_color, scaled_pts, 0)
-                pygame.draw.polygon(surface, OUTLINE, scaled_pts, 1)
+                pygame.draw.polygon(surface, myappdatac.OUTLINE, scaled_pts, 1)
             else:
                 pygame.draw.polygon(surface, bg_color, scaled_pts, 0)
-                pygame.draw.polygon(surface, OUTLINE, scaled_pts, 1)
+                pygame.draw.polygon(surface, myappdatac.OUTLINE, scaled_pts, 1)
 
 # -------- Utility-Funktionen --------
-def transform_coords(points, scale=SCALE, offset=OFFSET):
+def transform_coords(points, scale=myappdatac.SCALE, offset=myappdatac.OFFSET):
     ox, oy = offset
     return [(int(x * scale + ox), int(-y * scale + oy)) for x, y in points]
 
@@ -196,10 +250,10 @@ def point_is_inside_shape(shape_data, point):
     return (min_part["type"] == "outer")
 
 # -------- Laden & Vorverarbeiten (einmal) --------
-sf = shapefile.Reader(SHP_PATH)
+sf = shapefile.Reader(myappdatac.SHP_PATH)
 raw_shapes = sf.shapes()
-scale = SCALE
-offset = OFFSET
+scale = myappdatac.SCALE
+offset = myappdatac.OFFSET
 
 shapes_data = []  # Liste mit Metadaten pro Shape
 for idx, shape in enumerate(raw_shapes):
@@ -259,38 +313,38 @@ shapes_data.sort(key=shape_sort_key)  # kleine oben (werden später gezeichnet)
 
 # -------- Pygame / GUI Setup --------
 pygame.init()
-screen = pygame.display.set_mode(WINDOW_SIZE)
+screen = pygame.display.set_mode(myappdatac.WINDOW_SIZE)
 pygame.display.set_caption("Kennst du die Schweizer Kantone?")
 
-manager = pygame_gui.UIManager(WINDOW_SIZE, theme_path='theme.json')
+manager = pygame_gui.UIManager(myappdatac.WINDOW_SIZE, theme_path='theme.json')
 
 label = pygame_gui.elements.UILabel(
-    relative_rect=pygame.Rect((10, WINDOW_SIZE[1] - 30), (400, 25)),
+    relative_rect=pygame.Rect((100, myappdatac.WINDOW_SIZE[1] - 80), (400, 25)),
     text="Klicke auf einen Kanton",
     manager=manager
 )
 
 # Buttons nahe den vier Ecken
 btn_top_left = pygame_gui.elements.UIButton(
-    relative_rect=pygame.Rect((30, 120), (120, 50)),
+    relative_rect=pygame.Rect((40, 120), (120, 50)),
     text="Frankreich",
     manager=manager,
     object_id="#btn_tl"
 )
 btn_top_right = pygame_gui.elements.UIButton(
-    relative_rect=pygame.Rect((WINDOW_SIZE[0]-140, 120), (120, 50)),
+    relative_rect=pygame.Rect((myappdatac.WINDOW_SIZE[0]-100, 80), (100, 50)),
     text="Österreich",
     manager=manager,
     object_id="#btn_tr"
 )
 btn_bottom_left = pygame_gui.elements.UIButton(
-    relative_rect=pygame.Rect((250, 0), (120, 50)),
+    relative_rect=pygame.Rect((270, 0), (120, 50)),
     text="Deutschland",
     manager=manager,
     object_id="#btn_bl"
 )
 btn_bottom_right = pygame_gui.elements.UIButton(
-    relative_rect=pygame.Rect((WINDOW_SIZE[0]-240, WINDOW_SIZE[1]-200), (120, 50)),
+    relative_rect=pygame.Rect((myappdatac.WINDOW_SIZE[0]-240, myappdatac.WINDOW_SIZE[1]-200), (120, 50)),
     text="Italien",
     manager=manager,
     object_id="#btn_br"
@@ -298,7 +352,7 @@ btn_bottom_right = pygame_gui.elements.UIButton(
 
 # Hilfe-Button in der Mitte
 btn_help = pygame_gui.elements.UIButton(
-    relative_rect=pygame.Rect((WINDOW_SIZE[0]-140, 120), (120, 50)),
+    relative_rect=pygame.Rect((myappdatac.WINDOW_SIZE[0]-170, 126), (110, 40)),
     text="Liechtenstein",
     manager=manager,
     object_id="#btn_help"
@@ -307,12 +361,16 @@ btn_help = pygame_gui.elements.UIButton(
 # Textbox für Hilfe (zuerst versteckt)
 help_box = None
 
+# compassrose
+
+kompass = compassrose(screen,(140,420),size=40)
+
 # Alte Standardgröße
 BASE_FONT_SIZE = 28
 
 # Neues Label für OID in der Mitte
 mid_label = pygame_gui.elements.UILabel(
-    relative_rect=pygame.Rect((-1000, -1000), (200, int(BASE_FONT_SIZE*1.4))),
+    relative_rect=pygame.Rect((-1000, -1000), (200, int(BASE_FONT_SIZE*1.2))),
     text="",
     manager=manager,
     object_id = pygame_gui.core.ObjectID(class_id='@friendly',
@@ -324,7 +382,7 @@ mid_label = pygame_gui.elements.UILabel(
 # Schriftgröße über den Font-Manager anpassen (falls nötig)
 #mid_label.set_font(pygame.font.SysFont(None, int(BASE_FONT_SIZE * 1.4)))
 
-mapimg=pygame.image.load(MAP_PATH)
+mapimg=pygame.image.load(myappdatac.MAP_PATH)
 
 clock = pygame.time.Clock()
 selected_oid = None
@@ -383,15 +441,15 @@ while running:
 
                 # altes Shape zurückskalieren
                 if selected_shape and selected_shape is not chosen_shape:
-                    start_animation(selected_shape, SCALE_DOWN)
+                    start_animation(selected_shape, myappdatac.SCALE_DOWN)
 
                 # neues Shape hochskalieren
                 selected_shape = chosen_shape
-                start_animation(chosen_shape, SCALE_UP)
+                start_animation(chosen_shape, myappdatac.SCALE_UP)
 
                 # Label-Text setzen
                 selected_oid = chosen_shape["oid"]
-                mid_label.set_text(f"{kantone[selected_oid]}")
+                mid_label.set_text(f"{myappdatac.shplabels_kantone[selected_oid]}")
 
                 # Label in die Mitte des Shapes verschieben
                 # Berechne Durchschnitt aller Centroids der outer-parts
@@ -416,17 +474,21 @@ while running:
 
     # Zeichnen (Shapes in sortierter Reihenfolge)
 
-    screen.fill(BG_COLOR)
-    screen.blit(mapimg, mappos)
+
+    screen.fill(myappdatac.BG_COLOR)
+    screen.blit(mapimg, myappdatac.mappos)
+    kompass.draw()
     for s in shapes_data:
         if s is hover_candidate:
-            draw_shape(screen, s, HOVER_FILL, BG_COLOR)
+            draw_shape(screen, s, myappdatac.HOVER_FILL, myappdatac.BG_COLOR)
         else:
-            draw_shape(screen, s, NORMAL_FILL, BG_COLOR)
+            draw_shape(screen, s, myappdatac.NORMAL_FILL, myappdatac.BG_COLOR)
     if selected_shape:
-        draw_shape(screen, selected_shape, HOVER_FILL, BG_COLOR)
+        draw_shape(screen, selected_shape, myappdatac.HOVER_FILL, myappdatac.BG_COLOR)
 
     # Label: falls ausgewählt, bleibt Sichtbar (pygame_gui bringt es auf den Screen)
+
+
     manager.update(time_delta)
     manager.draw_ui(screen)
     update_animations()
